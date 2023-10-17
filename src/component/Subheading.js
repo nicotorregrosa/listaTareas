88e93ac2e86f80ebd475ab0e318631c35948e9a5
Nico <nico_torregrosa@hotmail.com>
2023-10-17 20:46:44 -0300

    add changes

---
+++ src/component/Subheading.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Subheading extends Component {
+  render() {
+    return <p className="text-center lead">Lista de tareas usando React</p>;
+  }
+}
